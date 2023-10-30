2764f4f69d1bccd83e6a1796718f409c841fe083
rstu491 <64621486+rstu491@users.noreply.github.com>
2023-10-30 22:45:10 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Try Out</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+  </head>
+  <body>
+    
+    <header class=" align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom ">
+      <!-- <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+      </div> -->
+
+      <ul class="nav col-8 col-md-auto mb-2 justify-content-center mb-md-0 ">
+        <li><a href="#home" class="nav-link px-2 link text-danger">Home</a></li>
+        <li><a href="./display.php" target="_blank" class="nav-link px-2 text-danger">Display Barang</a></li>
+        <li><a href="./kalkulator.php" target="_blank" class="nav-link px-2 text-danger">Kalkulator Barang</a></li>
+      </ul>
+    </header>
+    <div class="home" >
+      <h1 align="center">Selamat Datang Pada Dashboard Admin</h1>
+      <p align="center">Klik pada salah satu Fitur Diatas</p>
+    </div>
+</html>
